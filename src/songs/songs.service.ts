d332f9f055f51f4c443abbee0a9e6531c6775640
Khong <hankhongg@gmail.com>
2025-03-29 14:39:46 +0700

    feat: add query builder for pagination

--- src/songs/songs.service.ts
+++ src/songs/songs.service.ts
@@ -15,14 +15,7 @@ export class SongsService {
     // inject the repository here
     @InjectRepository(Song)
     private readonly songRepository: Repository<Song>;
-    // inject the connection here
 
-    private readonly songs: any[] = [];
-
-    // create(createSongDTO) {
-    //     this.songs.push(createSongDTO);
-    //     return createSongDTO;
-    // }
 
     async create(songDTO : CreateSongDTO) : Promise<Song> { // dùng async để gọi hàm bất đồng bộ trả await nhưng vẫn là promise
         const song = new Song(); // create a new song object
@@ -34,11 +27,9 @@ export class SongsService {
         song.lyrics = songDTO.lyrics;
         // save the song to the database
         return await this.songRepository.save(song); // save the song to the database
-        // return this.songs.push(songDTO); // save the song to the array
     }
 
     async findAll() : Promise<Song[]> { // return a promise of the array of songs
-        // return this.songs; // return the array of songs
         return await this.songRepository.find(); // find all songs in the database
     }
 
@@ -53,30 +44,21 @@ export class SongsService {
     }
 
     async update(id: number, updateDTOSong: UpdateSongDTO) : Promise<UpdateResult> { // return a promise of the update result
-        /*
-        const song = await this.findOne(id); // find the song by id
-        if (!song) { // check if the song exists
-            return null;
-        }
-        return await this.songRepository.save({...song, ...updateDTOSong}); // merge the song with the update data and save it to the database
-        */
         // update the song with the new data
         return await this.songRepository.update(id, updateDTOSong); // update the song and return it
         
     }
 
     async delete(id: number): Promise<DeleteResult> { // return a promise of the delete result
-        /*
-        const song = await this.findOne(id); // nhớ thêm await
-        if (!song) {
-            return null;
-        }
-        return await this.songRepository.delete(id); // delete the song from the database
-        */
        return await this.songRepository.delete(id); // delete the song from the database
     }
 
     paginate(options: IPaginationOptions) : Promise<Pagination<Song>>{
-        return paginate<Song>(this.songRepository, options); // paginate the songs
+
+        // thêm query builder để tìm kiếm theo tên bài hát / thời gian
+        const queryBuilder = this.songRepository.createQueryBuilder('songQuery'); // create a query builder for the song entity
+        // thêm điều kiện tìm kiếm theo tên bài hát
+        queryBuilder.orderBy('songQuery.releasedDate', 'DESC'); // order by release date descending
+        return paginate<Song>(queryBuilder, options); // paginate the songs
     }
 }
