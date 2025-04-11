4fe54dcd1f113a26a7c01b344966f649e5ac8cfb
Khong <hankhongg@gmail.com>
2025-04-11 19:23:09 +0700

    chore: add axios dep

--- db/migrations/1743615551334-add-phone-number.ts
+++ db/migrations/1743615551334-add-phone-number.ts
@@ -4,7 +4,7 @@ export class AddPhoneNumber1743615551334 implements MigrationInterface {
     name = 'AddPhoneNumber1743615551334'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "users" ADD "phone" character varying`);
+        await queryRunner.query(`ALTER TABLE "users" ADD "phone" character varying NULL`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
